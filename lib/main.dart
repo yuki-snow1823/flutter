b28c336c465370051bb45ae6bbbf9f985ec4bc9c
yuki-snow1823 <magic.iroha@gmail.com>
2020-12-17 01:21:32 +0900

    九とボックスと型定義

--- lib/main.dart
+++ lib/main.dart
@@ -27,75 +27,84 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  var _message;
-  final controller = TextEditingController();
+    String _message;
+    String _selected = 'One';
 
 
-  @override
-  void initState() {
-    _message = 'ok.';
-    super.initState();
-  }
+    @override
+    void initState() {
+      _message = 'ok.';
+      super.initState();
+    }
 
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('App Name'),
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('App Name'),
         ),
-      body:
-        Center(
-          child:
-            Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              mainAxisSize: MainAxisSize.max,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[ // 配置
-
-                Padding(
-                  padding: EdgeInsets.all(20.0),
-                  child: Text(
-                  _message,
-                    style: TextStyle(fontSize:32.0,
-                    fontWeight: FontWeight.w400,
-                    fontFamily: "Roboto"),
-                  ),
-                ),
+        body:
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
 
 
-                Padding(
-                  padding: EdgeInsets.all(10.0),
-                    child: TextField(
-                      controller: controller, // コントローラー？
-                      style: TextStyle(fontSize:28.0,
-                      color: const Color(0xFFFF0000),
-                      fontWeight: FontWeight.w400,
-                      fontFamily: "Roboto"),
-                  ),
-                ),
-  
-                FlatButton(
-                  padding: EdgeInsets.all(10.0),
-                  color: Colors.lightBlueAccent,
-                  child: Text(
-                        "Push me!",
-                        style: TextStyle(fontSize:32.0,
-                        color: const Color(0xFF000000),
-                        fontWeight: FontWeight.w400,
-                        fontFamily: "Roboto"),
-                      ),
-                  onPressed: buttonPressed
-                ),
-              ],
+            children: <Widget>[
+
+
+              Text(
+              _message,
+                style: TextStyle(fontSize:32.0,
+                fontWeight: FontWeight.w400,
+                fontFamily: 'Roboto'),
+              ),
+
+
+              Padding(
+                padding: EdgeInsets.all(10.0),
+              ),
+    
+              DropdownButton<String>(
+                onChanged: (String value)=> popupSelected(value),
+                value: _selected,
+                style: TextStyle(color:Colors.black,
+                  fontSize:28.0,
+                  fontWeight: FontWeight.w400,
+                  fontFamily: 'Roboto'),
+
+
+                items: <DropdownMenuItem<String>>[
+
+
+                  const DropdownMenuItem<String>(value: 'One',
+                    child: const Text('One')),
+
+
+                  const DropdownMenuItem<String>(value: 'Two',
+                    child: const Text('Two')),
+
+
+                  const DropdownMenuItem<String>(value: 'Three',
+                    child: const Text('Three')),
+                ],
+              ),
+    
+            ],
+
+
           ),
-        ),
-    );
-  }
-  void buttonPressed(){
-    setState((){
-      _message = 'you said: ' + controller.text;
-    });
-  }
-        
+      );
+    }
+
+
+    void popupSelected(String value){
+      setState(() {
+        _selected = value;
+        _message = 'select: $_selected';
+      });
+    }
+
+
 }
