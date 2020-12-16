42ac2554545a81ebeb93e8b30bfc8a8f4ce5fbb1
yuki-snow1823 <magic.iroha@gmail.com>
2020-12-17 01:01:32 +0900

    コントローラーの理解

--- lib/main.dart
+++ lib/main.dart
@@ -1,93 +1,3 @@
-// import 'package:flutter/material.dart';
-
-// void main() => runApp(new MyApp());
-
-// class MyApp extends StatelessWidget {
-//   final title = 'Flutterサンプル';
-//   final message = 'サンプル・メッセージ。';
-
-//   @override
-//   Widget build(BuildContext context) {
-
-//     return new MaterialApp(
-//       title: 'Flutter Demo',
-//       home: new MyHomePage(
-//         title: this.title
-//       ),
-//     );
-//   }
-// }
-
-// class MyHomePage extends StatefulWidget {
-
-//   MyHomePage({this.title}): super();
-
-//   final String title;
-
-//   @override
-//   _MyHomePageState createState() => new _MyHomePageState();
-// }
-
-// // データ用クラス
-// class Data {
-//   int _price;
-//   String _name;
-
-//   Data(this._name, this._price): super();
-
-//   @override
-//   String toString() {
-//     return _name + ':' + _price.toString() + '円';
-//   }
-// }
-
-// class _MyHomePageState extends State<MyHomePage> {
-//   // サンプルデータ
-//   static final _data = [
-//     Data('Apple',200),
-//     Data('Orange', 150),
-//     Data('Peach', 300)
-//   ];
-//   Data _item;
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     _item = _data[0];
-//   }
-
-//   void _setData() {
-//     setState(() {
-//       _item = (_data..shuffle()).first;
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-
-//     return Scaffold(
-
-//       appBar: AppBar(
-//         title: Text(widget.title),
-//       ),
-
-//       body: Text(
-//         _item.toString(),
-//         style: TextStyle(fontSize:32.0),
-//        ),
-
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: _setData,
-//         tooltip: 'set message.',
-//         child: Icon(Icons.star),
-//       ),
-
-//     );
-
-//   }
-
-// }
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -105,7 +15,7 @@ class MyApp extends StatelessWidget {
         accentColor: const Color(0xFF9c27b0),
         canvasColor: const Color(0xFFfafafa),
       ),
-      home: new MyHomePage(),
+      home: new MyHomePage(), //ステートレスなインスタンス作成
     );
   }
 }
@@ -113,7 +23,7 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key}) : super(key: key);
   @override
-  _MyHomePageState createState() => new _MyHomePageState();
+  _MyHomePageState createState() => new _MyHomePageState(); //ここでステートありにしている？
 }
 
 class _MyHomePageState extends State<MyHomePage> {
@@ -141,7 +51,7 @@ class _MyHomePageState extends State<MyHomePage> {
               mainAxisAlignment: MainAxisAlignment.start,
               mainAxisSize: MainAxisSize.max,
               crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
+              children: <Widget>[ // 配置
 
                 Padding(
                   padding: EdgeInsets.all(20.0),
@@ -157,7 +67,7 @@ class _MyHomePageState extends State<MyHomePage> {
                 Padding(
                   padding: EdgeInsets.all(10.0),
                     child: TextField(
-                      controller: controller,
+                      controller: controller, // コントローラー？
                       style: TextStyle(fontSize:28.0,
                       color: const Color(0xFFFF0000),
                       fontWeight: FontWeight.w400,
